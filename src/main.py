5e14ac11a2e50207cbfff76f51efe467a5371c00
AriaCoder <aria@tancestry.com>
2023-07-11 10:31:18 -0700

    Bot drives like crazzzy

---
+++ src/main.py
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       aria                                                         #
+# 	Created:      7/11/2023, 9:55:29 AM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain = Brain()
+leftMotor = Motor(Ports.PORT1)
+rightMotor = Motor(Ports.PORT6)
+
+leftMotor.set_velocity(100, PERCENT)
+rightMotor.set_velocity(30, PERCENT)
+
+leftMotor.spin(DirectionType.FORWARD)
+rightMotor.spin(DirectionType.REVERSE)
+
+while True:
+    if leftMotor.velocity() > 0:
+        print(leftMotor.velocity())
+    sleep(2, SECONDS)
